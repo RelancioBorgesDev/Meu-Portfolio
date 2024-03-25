6923dd28f6a4801170634e5955b981ad76e4a7f9
RelancioBorgesDev <relancio.dev@gmail.com>
2024-03-25 11:47:47 -0300

    feat: add estrutura pagina blog

---
+++ src/components/Tag.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface TagProps {
+  tag: string;
+}
+
+export default function Tag({ tag }: TagProps) {
+  return <div className="bg-tag p-2 rounded-lg text-sm">{tag}</div>;
+}
