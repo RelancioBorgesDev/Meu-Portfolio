1b938ca0fe91e02af3e6f65fbdff48786276e0aa
RelancioBorgesDev <relancio.dev@gmail.com>
2024-03-25 10:06:16 -0300

    chore: ajustes, removido icons

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,28 +1,19 @@
-import type { ElementType } from "react";
 
 interface NavLinkProps {
   link: string;
   link_title: string;
-  icon: ElementType;
   pathname: string;
 }
 
 export default function NavLink({
   link,
   link_title,
-  icon: Icon,
   pathname,
 }: NavLinkProps) {
   let pathNameIsEqual = pathname == link;
   return (
     <li>
-      <a href={link} className="flex items-center gap-2">
-        <Icon
-          size={`${pathNameIsEqual ? 32 : 10}`}
-          className={`${
-            pathNameIsEqual ? "text-2xl font-bold" : "text-xl text-gray-300/95"
-          }`}
-        />
+      <a href={link} className="flex items-center gap-2 w-fit">
         <span
           className={`${
             pathNameIsEqual ? "text-2xl font-bold" : "text-xl text-gray-300/95"
--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,9 +1,7 @@
-import type { ElementType } from "react";
 import Button from "./Button";
 
 interface SectionHeaderProps {
   title: string;
-  icon: ElementType;
   hasAll: boolean;
   link?: string;
 }
@@ -11,14 +9,12 @@ interface SectionHeaderProps {
 export default function SectionHeader({
   title,
   hasAll,
-  icon: Icon,
   link,
 }: SectionHeaderProps) {
   return (
     <header className="border-b-2 py-2 border-slate-700/40 flex items-center justify-between">
       <div className="flex items-center gap-2">
-        <Icon size={32} />
-        <span className="text-xl">{title}</span>
+        <span className="text-2xl font-bold">{title}</span>
       </div>
       {hasAll && (
         <a href={"#"}>
