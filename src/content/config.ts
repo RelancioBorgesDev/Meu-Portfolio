be9dd520de79a0d8115d369d7ac1dd5222998706
RelancioBorgesDev <relancio.dev@gmail.com>
2024-03-27 15:07:51 -0300

    refactor: removido schemas

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,36 @@
 import { defineCollection } from "astro:content";
-import { postSchema, projectSchema } from "../schemas/schemas";
+import { z } from "astro/zod";
 
 const projectCollection = defineCollection({
   type: "content",
-  schema: projectSchema,
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      projectType: z.string(),
+      video: z.string(),
+      tags: z.array(z.string()),
+      date: z.date(),
+      card_image: image(),
+      featured: z.boolean(),
+    }),
 });
 const postCollection = defineCollection({
   type: "content",
-  schema: postSchema,
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      author: z.string(),
+      tags: z.array(z.string()),
+      date: z.date(),
+      card_image: image(),
+      banner_image: image(),
+      featured: z.boolean(),
+    }),
 });
 
 export const collections = {
-  "project": projectCollection,
-  "post": postCollection,
+  project: projectCollection,
+  post: postCollection,
 };
